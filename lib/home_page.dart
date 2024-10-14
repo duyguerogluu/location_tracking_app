86135cef139786e618496277005d0f94e18f1de5
duyguerogluu <erogluduyguduygu@gmail.com>
2024-10-14 17:21:16 +0300

    geolocator functions

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isLoadingFinish = false;
+  bool requiredPermission = false;
+  Position? currentLocation;
+
+  Future<Position> getLocation() async {
+    print("getLocationstart");
+    var perm = await Geolocator.checkPermission();
+    if (perm == LocationPermission.denied) {
+      return Future.error('Konum izni reddedildi');
+    }
+    return await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.best)
+        .catchError((e) {
+      print('error $e');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    void permissionOK() {
+      getLocation().then((pos) {
+        isLoadingFinish = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [],
+    );
   }
 }
