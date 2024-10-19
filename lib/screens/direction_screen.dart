b1dababc4ae166bea9f18b489dea88cbc63b87a6
duyguerogluu <erogluduyguduygu@gmail.com>
2024-10-19 23:11:31 +0300

    direction screen

---
+++ lib/screens/direction_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class DirectionScreen extends StatefulWidget {
+  const DirectionScreen({super.key});
+
+  @override
+  State<DirectionScreen> createState() => _DirectionScreenState();
+}
+
+class _DirectionScreenState extends State<DirectionScreen> {
+  final LatLng _center = const LatLng(41.406907598969205, 2.173116555622275);
+  final LatLng startLocation =
+      const LatLng(41.40902647035469, 2.2008270607482556);
+  final LatLng finishLocation =
+      const LatLng(41.440519653383596, 2.2451869794393846);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Direction Screen'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 7.0,
+        ),
+      ),
+    );
+  }
+}
