6b10b7402661a98c828a1f1c47e4ca51158140e8
duyguerogluu <erogluduyguduygu@gmail.com>
2024-10-24 23:42:00 +0300

    profile screen

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Profil Sayfası')),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              children: [
+                const Text(
+                  'Kullanıcı Bilgileri',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const CircleAvatar(radius: 40),
+                const SizedBox(height: 16),
+                const Text('Ad: John Doe'),
+                const Text('E-posta: johndoe@example.com'),
+                const Text('Telefon: +90 123 456 7890'),
+                const SizedBox(height: 32),
+                const Text(
+                  'Ödeme Bilgileri',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16),
+                const Text('Kredi Kartı: **** **** **** 1234'),
+                const Text('Son Kullanma Tarihi: 12/25'),
+                const Text('CVV: ***'),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Ödeme Bilgilerini Güncelle'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
